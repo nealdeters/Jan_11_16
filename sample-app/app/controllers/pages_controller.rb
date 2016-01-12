0ad9da6a0035349d7ce4690b2cca53d6b3f2d47c
nealdeters <nealdeters@gmail.com>
2016-01-11 20:27:44 -0600

    creating github folder

--- sample-app/app/controllers/pages_controller.rb
+++ sample-app/app/controllers/pages_controller.rb
@@ -1,17 +1,20 @@
 class PagesController < ApplicationController
   def home
-
+    @current_time = Time.now.strftime "%l:%M %P %Y"
+    @sum = 1 + 1
+    @title = 'home'
   end
 
   def talk
-
+    @title = 'bubbles'
+  
   end
 
   def page1
-
+    @title = 'page1'
   end
 
   def page2
-
+    @title = 'page2'
   end
 end
--- sample-app/config/routes.rb
+++ sample-app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/' => 'pages#index'
+  get '/' => 'pages#home'
   get '/page1' => 'pages#page1'
   get '/page2' => 'pages#page2'
   get '/talk' => 'pages#talk'
